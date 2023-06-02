63ee1be6a21f5113b531266bb36576bc38700433
Sami Boumaiza <sboumaiz@ptb-11108284.saclay.inria.fr>
2023-06-02 11:18:43 +0200

    parametrization succeded in some way

--- tests/test_rat_lab.py
+++ tests/test_rat_lab.py
@@ -2,7 +2,17 @@
     My Pytest rat lab
 """
 
+from pprint import pprint
+import itertools
 import pytest
+import numpy as np
+from numpy.random import default_rng
+
+# export PYTHONPATH="/home/sboumaiz/Bureau/stage_mediation"
+from med_bench.src.get_simulated_data import simulate_data
+
+
+# ATTEMPT TO MERGE PARAMETRIZATION AND TESTS
 
 a = 1
 
@@ -15,47 +25,27 @@ def fix(b):
 
 @pytest.fixture(autouse=True)
 def resultat():
-    pass
+    return 3.14 * a
 
 
 def test_demo():
-    assert d
-    # try:
-    #     print(a)
-    # except:
-    #     print("An exception occurred")
-    # assert resultat(1) == 0
-    pass
+    assert fix(2) == 4
 
 
-from pprint import pprint
-import itertools
-import pytest
-import numpy as np
-from numpy.random import default_rng
-
-# export PYTHONPATH="/home/sboumaiz/Bureau/stage_mediation"
-from med_bench.src.get_simulated_data import simulate_data
-
-
-# @pytest.fixture(autouse=True)
-# def data():
-#     return simulate_data(10, default_rng(7))
+# def test_demo0():
+#     assert a
+#     try:
+#         print(pi)
+#     except:
+#         print("An exception occurred")
+#     assert resultat() == 3.14
 
 
-# @pytest.fixture(autouse=True)
-# def effects():
-#     return np.array(data[4:9])
-
-
-@pytest.mark.parametrize.fixture(autouse=True)
-def fix():
-    pass
-
-
+# CHECK WHETHER A LIST IS NEDEED
+# use directly itertools, param_list useless?
 pamarameter_list = []
 for pamarameter in itertools.product(
-    [1, 2, 100],
+    [1, 2, 10],
     [default_rng(1), default_rng(10)],
     [False, True],
     [False, True],
@@ -69,114 +59,61 @@ for pamarameter in itertools.product(
     [1, 0.1, 10],
 ):
     pamarameter_list.append(pamarameter)
-# use directly itertools, param_list useless?
 
 
-data = None
-err_returned = True
-
-
-@pytest.fixture(autouse=True)
-def setup(
-    n,
-    rg,
-    mis_spec_m,
-    mis_spec_y,
-    dim_x,
-    dim_m,
-    seed,
-    type_m,
-    sigma_y,
-    sigma_m,
-    beta_t_factor,
-    beta_m_factor,
-):
+for param in pamarameter_list:
     try:
-        err_returned = False
-        data = simulate_data(
-            n,
-            rg,
-            mis_spec_m,
-            mis_spec_y,
-            dim_x,
-            dim_m,
-            seed,
-            type_m,
-            sigma_y,
-            sigma_m,
-            beta_t_factor,
-            beta_m_factor,
-        )
-    except ValueError as err:
-        err_returned = True
-
-
-@pytest.mark.parametrize(
-    [
-        "n",
-        "rg",
-        "mis_spec_m",
-        "mis_spec_y",
-        "dim_x",
-        "dim_m",
-        "seed",
-        "type_m",
-        "sigma_y",
-        "sigma_m",
-        "beta_t_factor",
-        "beta_m_factor",
-    ],
-    pamarameter_list,
-)
-class ParametrizedTest:
-    def test_total_is_direct_plus_indirect(self):
-        assert effects[0] == effects[1] + effects[4]  # total = theta_1 + delta_0
-        assert effects[0] == effects[2] + effects[3]  # total = theta_0 + delta_1
-
-    def test_effects_are_equals_if_y_well_specified(self):
+        data = simulate_data(*param)
+    except:
         pass
+    else:
+
+        def test_dimension_x():
+            assert (data[0]).shape == (param[0], param[4])  # (n, dim_x)
 
 
-
-data = simulate_data(
-    n=1,
-    rg=default_rng(42),
-    mis_spec_m=False,
-    mis_spec_y=False,
-    dim_x=1,
-    dim_m=1,
-    seed=1,
-    type_m="continuous",
-    sigma_y=0.5,
-    sigma_m=0.5,
-    beta_t_factor=1,
-    beta_m_factor=10000,
-)
-print(data)
-
-# effects = np.array(data[4:9])
-# print(effects)
+# CONCLUSION : Je peux m'en sortir avec une boucle for et *param, mais c'est pas hyper élégant
 
 
+# # GET HERE SOME SIMULATE_DATA OUTPUT
+
+# data = simulate_data(
+#     n=1,
+#     rg=default_rng(42),
+#     mis_spec_m=False,
+#     mis_spec_y=False,
+#     dim_x=1,
+#     dim_m=1,
+#     seed=1,
+#     type_m="continuous",
+#     sigma_y=0.5,
+#     sigma_m=0.5,
+#     beta_t_factor=1,
+#     beta_m_factor=10000,
+# )
+# print(data)
+
+# # effects = np.array(data[4:9])
+# # print(effects)
 
 
-
-data=[]
-try:
-    data = simulate_data(
-        n=3,
-        rg=default_rng(42),
-        mis_spec_m=False,
-        mis_spec_y=False,
-        dim_x=1,
-        dim_m=1,
-        seed=1,
-        type_m="binary",
-        sigma_y=0.5,
-        sigma_m=0.5,
-        beta_t_factor=10,
-        beta_m_factor=1)
-except ValueError as err:
-    print(err)
-else:
-    print(data)
+# data = []
+# try:
+#     data = simulate_data(
+#         n=3,
+#         rg=default_rng(42),
+#         mis_spec_m=False,
+#         mis_spec_y=False,
+#         dim_x=1,
+#         dim_m=1,
+#         seed=1,
+#         type_m="binary",
+#         sigma_y=0.5,
+#         sigma_m=0.5,
+#         beta_t_factor=10,
+#         beta_m_factor=1,
+#     )
+# except ValueError as err:
+#     print(err)
+# else:
+#     print(data)
