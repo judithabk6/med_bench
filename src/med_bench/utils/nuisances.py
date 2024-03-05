b0a1dddb27a3ba6628f00c06118904cf2a8a3df6
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2024-03-05 12:03:34 +0100

    add random_state to scikit_learn functions

--- src/med_bench/utils/nuisances.py
+++ src/med_bench/utils/nuisances.py
@@ -81,7 +81,7 @@ def _get_classifier(regularization, forest, calibration, random_state=42):
         clf = RandomForestClassifier(random_state=random_state,
                                      n_estimators=100, min_samples_leaf=10)
     if calibration in {"sigmoid", "isotonic"}:
-        clf = CalibratedClassifierCV(clf, method=calibration)
+        clf = CalibratedClassifierCV(clf, method=calibration, random_state=random_state)
 
     return clf
 
@@ -97,9 +97,9 @@ def _get_regressor(regularization, forest, random_state=42):
     _, alphas = _get_regularization_parameters(regularization)
 
     if not forest:
-        reg = RidgeCV(alphas=alphas, cv=CV_FOLDS)
+        reg = RidgeCV(alphas=alphas, cv=CV_FOLDS, random_state=random_state)
     else:
-        reg = RandomForestRegressor(n_estimators=100, min_samples_leaf=10)
+        reg = RandomForestRegressor(n_estimators=100, min_samples_leaf=10, random_state=random_state)
 
     return reg
 
